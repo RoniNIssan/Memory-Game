4202b0de002c29ba410f299efeece76aa0599056
Roni Nissan <roninissan07@gmail.com>
2022-07-10 17:37:54 +0300

    writing input to intro screen done!

--- client.py
+++ client.py
@@ -13,9 +13,10 @@ ready_to_start = False
 def handle_graphics():
     global ready_to_start
 
-
     # define screen size
     WINDOW_SIZE = (854, 480)
+    clock = pygame.time.Clock()
+    clock.tick(25)
     pygame.init()
     screen = pygame.display.set_mode(WINDOW_SIZE)
 
@@ -47,11 +48,6 @@ def show_intro_screen(screen):
     ip_rect = pygame.Rect(327, 354, 196, 34)
     play_rect = pygame.Rect(339, 431, 176, 39)
 
-    # type - in user font
-    base_font = pygame.font.Font(None, 16)
-    user_text = ""
-    color = pygame.Color("White")
-
     # user input
     writing_username = False
     writing_port = False
@@ -59,6 +55,9 @@ def show_intro_screen(screen):
     username = ""
     port = ""
     ip = ""
+
+    # type - in user font
+    base_font = pygame.font.Font(None, 25)
 
     while True:
         pygame.display.update()
@@ -88,18 +87,26 @@ def show_intro_screen(screen):
             if event.type == pygame.KEYDOWN:
                 print("key")
                 if writing_username:
-                    username += keyboard_input(event)
+                    # TODO: everytime when writing - reset rect color to original and rewrite (sort of erasing)
+                    username = keyboard_input(event, username)
+                    username_text_surface = base_font.render(username, True, (255, 255, 255))
+                    screen.blit(username_text_surface, (username_rect.x + 5, username_rect.y + 5))
                     print("username:" + username)
                 elif writing_port:
-                    port += keyboard_input(event)
+                    port = keyboard_input(event, port)
+                    port_text_surface = base_font.render(port, True, (255, 255, 255))
+                    screen.blit(port_text_surface, (port_rect.x + 5, port_rect.y + 5))
                     print("port:" + port)
                 elif writing_ip:
-                    ip += keyboard_input(event)
+                    ip = keyboard_input(event, ip)
+                    ip_text_surface = base_font.render(ip, True, (255, 255, 255))
+                    screen.blit(ip_text_surface, (ip_rect.x + 5, ip_rect.y + 5))
                     print("ip:" + ip)
 
+                pygame.display.flip()
 
-def keyboard_input(event):
-    user_text = ""
+
+def keyboard_input(event, user_text):
     if event.key == pygame.K_BACKSPACE:
 
         # get text input from 0 to -1 i.e. end.
